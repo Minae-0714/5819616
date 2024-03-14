c544d4eab8b9ca779cde40c513df29006f981d5f
Minae-0714 <lmine0714l@gmail.com>
2024-03-14 23:52:39 +0900

    0312_5819616

---
+++ 0312_5819616.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+    int sum = 0;
+    int prime;
+
+    for (int num = 2; num <= 100; num++) {
+        prime = 1;
+
+        for (int j = 2; j <= num / 2; j++) {
+            if (num % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+
+        if (prime == 1) {
+            sum += num;
+        }
+    }
+
+    printf("Sum : %d\n", sum);
+}
